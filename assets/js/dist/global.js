b597e76021f4130717823ba96f465673de91ea50
Ed Williams <ed@rosielees.co.uk>
2016-04-04 17:54:37 +0100

    added; opening page

--- assets/js/dist/global.js
+++ assets/js/dist/global.js
@@ -9,6 +9,16 @@ var global = (function(){
 
     function events() {
 
+        $(window).on('load', function() {
+            setTimeout(function() {
+                window.scrollTo(0,0);
+            }, 0);
+        });
+
+        $('.name, .title').on('click', function() {
+            $('html').removeClass('closed');
+        })
+
     }
 
     function lazyLoad() {
