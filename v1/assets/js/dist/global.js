569d6096a928df72963900486dfca1fedeb9489d
Ed Williams <Ed.Williams@passivsystems.com>
2018-02-08 21:05:40 +0000

    added; missed files

---
+++ v1/assets/js/dist/global.js
@@ -0,0 +1,38 @@
+var global = (function(){
+
+    function init() {
+
+        events();
+        lazyLoad();
+
+    }
+
+    function events() {
+
+        // $(window).on('load', function() {
+        //     setTimeout(function() {
+        //         window.scrollTo(0,0);
+        //     }, 0);
+        // });
+
+        $('.name, .title').on('click', function() {
+            $('html').removeClass('closed');
+        });
+
+        if ( window.matchMedia('(max-width: 1024px)').matches ) {
+            $('html').removeClass('closed');
+        }
+
+    }
+
+    function lazyLoad() {
+
+        $('img.lazy').lazyload({
+            effect : 'fadeIn'
+        });
+
+    }
+
+    init();
+
+})();
