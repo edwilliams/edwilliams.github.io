26eb5ab57d4d5e9241613fc0d25ac56f33cae243
Ed Williams <Ed.Williams@passivsystems.com>
2018-02-10 08:36:27 +0000

    added; social links

--- src/components/Partials/Footer/index.js
+++ src/components/Partials/Footer/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import createReactClass from 'create-react-class'
 
+var style = () => `
+  .footer {
+    border-top: 1px solid #494C4E;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
+`
+
 var Footer = createReactClass({
   render() {
     return (
       <footer className="footer">
+        <style>{style()}</style>
         <div className="plm pvs">© Ed Williams. All rights reserved.</div>
       </footer>
     )
