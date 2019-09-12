fa0d0244b1fd1be9b3664f6b59875cdea2806acf
Ed Williams <edwilliamsdevelopment@gmail.com>
2019-09-12 15:52:49 +0100

    edit; styling

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,7 +6,7 @@ import Item from '../Partials/Item'
 
 const style = () => `
 .container-small {
-  max-width: 768px;
+  max-width: 1024px;
   margin: 0 auto;
 }
 @media screen and (max-width: 600px) {
