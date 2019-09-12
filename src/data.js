6bfb43e29f0c9f040e32e77ad861581d920bdcce
Ed Williams <edwilliamsdevelopment@gmail.com>
2019-09-12 15:53:07 +0100

    updated; CV and About

--- src/data.js
+++ src/data.js
@@ -6,11 +6,11 @@ export default {
   website: 'edwilliamsdevelopment.com',
   about: [
     'As a Frontend Engineer and Full Stack Developer, I have many years of experience in building robust and scalable websites, webapps and PWA’s. As well as solid commercial experience of building full-stack applications.',
-    'As a contractor I enjoy taking the lead in projects where ever possible and have experience working solo / remotely and in small teams mentoring other developers. I always work to high standards and deliver maintainable and test driven software while keeping the end user in mind at all times.',
-    'I have consulted for enterprises of all sizes (PassivSystems, Jaguar Land Rover, Good Energy, et al) and enjoy communicating with the client and all stakeholders throughout a project.',
+    'As a contractor I enjoy taking the lead in projects wherever possible and have experience working solo / remotely and mentoring other developers. I always work to high standards and deliver maintainable and test driven software while keeping the end user in mind at all times.',
+    'I have consulted for enterprises of all sizes (Discover, RAC, PassivSystems, Jaguar Land Rover, Good Energy, et al) and enjoy communicating with the client and all stakeholders throughout a project.',
     'Specialising in React / Redux has been a natural fit for my style of development as I favour a functional / declarative approach to programming and always seeks to lift as much of a codebase into pure functions as possible, where it can be written into utilities / libraries and tested.',
     'As a UI expert I have extensive experience with animations (CSS, SVG and WebGL) and feel equally at home styling interfaces with JavaScript as I do with CSS / SASS. I have also become a keen advocate of PWA’s and conform to best practices (e.g. responsive design, cross browser development) whilst retaining solid commercial experience of Cordova and App store deployment.',
-    'Having worked end-to-end on many large projects I am comfortable taking on all of the above alongside other responsibilities, such as writing CI build scripts (Bash / Webpack / Gulp / Grunt), designing databases (MySQL / DynamoDB), architecting API’s (Node / AWS / Heroku / PHP), keeping fine grain version control (Git) of codebases on the command line and working in multiple environments (Linux, OSX and Windows)',
+    'Having worked end-to-end on many large projects I am comfortable taking on all of the above alongside other responsibilities, such as writing CI build scripts (Bash / Webpack / Gulp / Grunt), designing databases (MySQL / DynamoDB / S3), architecting API’s (Node / AWS / Heroku / PHP), keeping fine grain version control (Git) of codebases on the command line and working in multiple environments (Linux, OSX and Windows)',
     'See below for a selective list the applications I have developed over recent years. For more info / links, please visit: edwilliamsdevelopment.com',
   ]
 }
