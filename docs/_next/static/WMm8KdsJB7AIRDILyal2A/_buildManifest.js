4ff5f25b8f119c51dd012247a58248544fb345fd
Ed Williams <edwilliamsdevelopment@gmail.com>
2021-05-21 12:22:35 +0100

    removed; docs from gitignore and move cname

---
+++ docs/_next/static/WMm8KdsJB7AIRDILyal2A/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-fed1e659d2693f3e25c4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d5291a74a0f4407fa8e0.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
